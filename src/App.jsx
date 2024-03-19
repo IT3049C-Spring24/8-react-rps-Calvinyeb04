684acda499b7be7359e840c763e515fe74f9ca4d
Calvin Yeboah <113629722+Calvinyeb04@users.noreply.github.com>
2024-03-19 01:47:47 -0400

    Refactor App component to use functional components and pass userName as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import './App.css';
-import GameView from './components/GameView';
-import WelcomeView from './components/WelcomeView';
+
+
+
+import React, { useState } from 'react'; // Import useState from 'react'
+import WelcomeView from './components/WelcomeView'; // Import WelcomeView component
+import GameView from './components/GameView'; // Import GameView component
 
 function App() {
+  const [userName, setUserName] = useState(""); // Declare userName state variable
+
   return (
-    <div class="container">
-      <h1 class="mainHeader">Rock Paper Scissors</h1>
-      <WelcomeView />
-      <GameView />
+    <div className="container"> {/* Change class to className */}
+      <h1 className="mainHeader">Rock Paper Scissors</h1> {/* Change class to className */}
+      {/* Pass userName and setUserName as props to WelcomeView component */}
+      <WelcomeView userName={userName} setUserName={setUserName} />
+      {/* Pass userName as prop to GameView component */}
+      <GameView userName={userName} />
     </div>
   );
 }
