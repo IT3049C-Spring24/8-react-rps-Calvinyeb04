e3a12c27fb9319ec3b80910a695cc4f3cf593f3a
Calvin Yeboah <113629722+Calvinyeb04@users.noreply.github.com>
2024-03-19 00:49:37 -0400

    Refactor App component to include GameView and WelcomeView components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import './App.css'
-import Button from './components/Button'
+import './App.css';
+import GameView from './components/GameView';
+import WelcomeView from './components/WelcomeView';
 
 function App() {
   return (
-    <>
-      <h1>RPS in React</h1>
-      <Button /> {/* This line here is just to show you how a component can be nested here*/}
-      {/* Add the Rest of your React Components here */}
-    </>
-  )
+    <div class="container">
+      <h1 class="mainHeader">Rock Paper Scissors</h1>
+      <WelcomeView />
+      <GameView />
+    </div>
+  );
 }
 
-export default App
+export default App;
